504215853bdc90eb5f69de34458f01f2e9ba9c08
Paul Brubaker <pbrubak@gmail.com>
2021-06-29 16:58:21 -0700

    fix spacing making two space indentation four spaces

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { countsAsYes } from './utils.js'; 
-  
+import { countsAsYes } from './utils.js';
+
 const launchQuiz = document.getElementById('click-me')
 const results = document.getElementById('results');
 
@@ -7,18 +7,17 @@ launchQuiz.addEventListener('click', () => {
     let userName = prompt('What is your name?');
     let wantsQuiz = confirm('Do you want to start the quiz?');
     if (!wantsQuiz) {
-      return
-    } 
+        return
+    }
     else {
-      let totalCorrect = 0;
-      let ans1 = prompt('Are filberts the best nut?');
-      if (countsAsYes(ans1)) totalCorrect++;
-      let ans2 = prompt('Do you love filberts?');
-      if (countsAsYes(ans2)) totalCorrect++;
-      let ans3 = prompt('Are roasted filberts the best kind of filbert?');
-      if (countsAsYes(ans3)) totalCorrect++;
-      alert('Quiz complete! Ready for your results?');
-      results.textContent = `Okay ${userName}, you got ${totalCorrect}/3 correct`;
+        let totalCorrect = 0;
+        let ans1 = prompt('Are filberts the best nut?');
+        if (countsAsYes(ans1)) totalCorrect++;
+        let ans2 = prompt('Do you love filberts?');
+        if (countsAsYes(ans2)) totalCorrect++;
+        let ans3 = prompt('Are roasted filberts the best kind of filbert?');
+        if (countsAsYes(ans3)) totalCorrect++;
+        alert('Quiz complete! Ready for your results?');
+        results.textContent = `Okay ${userName}, you got ${totalCorrect}/3 correct`;
     }
 })
-  
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-export function countsAsYes(word){
+export function countsAsYes(word) {
     let lowerCaseWord = word.toLowerCase();
     if (lowerCaseWord[0] === 'y') {
         return true;
