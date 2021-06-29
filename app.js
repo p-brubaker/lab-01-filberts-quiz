0a2da9bb82f35fc250615c2ac30582befebda798
Paul Brubaker <pbrubak@gmail.com>
2021-06-29 15:20:01 -0700

    quiz questions added and working

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const launchQuiz = document.getElementById('click-me')
   // add "click" event listener to the button
 launchQuiz.addEventListener('click', () => {
     let wantsQuiz = confirm('Do you want to start the quiz?');
-    console.log(wantsQuiz);
+    if (!wantsQuiz) {
+        return
+    } else {
+        let ans1 = prompt('Are filberts the best nut?');
+        let ans2 = prompt('Do you love filberts?');
+        let ans3 = prompt('Are roasted filberts the best kind of filbert?');
+        console.log(ans1,ans2,ans3);
+    }
 })
   //   confirm the user wants to start the quiz
   //   if they do -- prompt for at least 3 questions and store the answer
