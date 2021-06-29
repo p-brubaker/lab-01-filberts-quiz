21ec9a371dc60c78854e23c3b54096043230e1c3
Paul Brubaker <pbrubak@gmail.com>
2021-06-29 14:46:26 -0700

    change HTML from dog to filbert

--- test/example.test.js
+++ test/example.test.js
@@ -1,49 +1,12 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { isYes } from '../utils.js';
+
 
 const test = QUnit.test;
 
 // expect can be called anything you want
 // its just a parameter
-test('should return true for "yes" strings', assert => {
-    // Arrange
-    const yesString = 'yes';
-    const expected = true;
-    // Act
-    const actual = isYes(yesString);
-    // Expect
-    assert.equal(actual, expected);
 
-});
-test('should return true for "y" strings', assert => {
-    // Arrange
-    const yesString = 'y';
-    const expected = true;
-    // Act
-    const actual = isYes(yesString);
-    // Expect
-    assert.equal(actual, expected);
-
-});
-
-test('should return true for uppercase YES strings', assert => {
-    // Arrange
-    const yesString = 'YES';
-    const expected = true;
-    // Act
-    const actual = isYes(yesString);
-    // Expect
-    assert.equal(actual, expected);
-
-});
-
-test('should return false for non yes-y string', expect => {
-    // Arrange
-    const no = 'no';
-    const expected = false;
-    // Act
-    const actual = isYes(no);
-    // Expect
-    expect.equal(actual, expected);
-});
+test('should return true given the input string <yes>', expect => {
+    
+})  
