e1632073d36f17179dcab93c1257441dec9a8934
Paul Brubaker <pbrubak@gmail.com>
2021-06-29 14:51:12 -0700

    launch quiz prompt working

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
   // get the button element
+const launchQuiz = document.getElementById('click-me')
 
-  const launchQuiz = document.getElementById('click-me')
   // add "click" event listener to the button
+launchQuiz.addEventListener('click', () => {
+    let wantsQuiz = confirm('Do you want to start the quiz?');
+    console.log(wantsQuiz);
+})
   //   confirm the user wants to start the quiz
   //   if they do -- prompt for at least 3 questions and store the answer
   //   for each answer, check if the user entered a correct value
