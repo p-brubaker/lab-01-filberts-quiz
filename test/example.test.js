789e263112430b4fc80739e890a3d8dda6e65733
Paul Brubaker <pbrubak@gmail.com>
2021-06-29 15:51:19 -0700

    failing all five tests

--- test/example.test.js
+++ test/example.test.js
@@ -15,3 +15,28 @@ test('should return true given the input string <yes>', (expect) => {
     expect.equal(actual, expected);
 });
 
+test('should return true given input <YES>', (expect) => {
+    const expected = true;
+    const actual = countsAsYes('YES');
+    expect.equal(actual, expected);
+});
+
+test('should return true given input <Yeperdee deperdee doodles>', (expect) => {
+    const expected = true; 
+    const actual = countsAsYes('Yeperdee deperdee doodles');
+    expect.equal(actual, expected);
+});
+
+test('should return false given input <no>', (expect) => {
+    const expected = false;
+    const actual = countsAsYes('no');
+    expect.equal(actual, expected);
+});
+
+test('should return false given input <NOPE>', (expect) => {
+    const expected = false;
+    const actual = countsAsYes('NOPE');
+    expect.equal(actual, expected);
+})
+
+
